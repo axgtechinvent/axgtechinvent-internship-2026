35d559130694bc0f435818725329d0130dc1379c
alexanghel17 <alexanghel734@gmail.com>
2026-08-06 21:57:09 +0300

    feat:implementation of the s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,18 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  #Implementation of the S3 backend
+  backend "s3" {
+      bucket         = "my-team-tfstate-bucket-2026"  # Bucket-ul S3 creat
+      key            = "dev/terraform.tfstate"        # Calea din S3 unde se va salva starea
+      region         = "eu-central-1"                 
+      use_lockfile = true        # Tabela pentru locking
+      encrypt        = true                           # Criptarea fișierului pe S3
+    }
 }
 
-# Configure the AWS Provider
+#Implementation of the AWS Provider
 provider "aws" {
   region = "eu-central-1"
   default_tags {
