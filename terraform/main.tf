8b034fd895ea5b58683cc074260fdd15cb1555f7
Norby <236401913+Nxrby12@users.noreply.github.com>
2026-08-13 13:21:10 +0300

    fix: workflow#2

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
 
 # 4. Regula Outbound (iesire permisa catre orice destinatie)
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
-resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.app_sg.id
   description       = "Allow all outbound traffic"
   cidr_ipv4         = "0.0.0.0/0"
